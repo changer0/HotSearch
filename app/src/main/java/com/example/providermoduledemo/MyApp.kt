249906433452b747c7477d1c43f44c81555e806d
zhanglulu <zhanglulu@yuewen.com>
2020-10-18 15:30:01 +0800

    [commit] update

--- app/src/main/java/com/example/providermoduledemo/MyApp.kt
+++ app/src/main/java/com/example/providermoduledemo/MyApp.kt
@@ -5,6 +5,8 @@ import com.qq.reader.appconfig.HeadInterceptor
 import com.qq.reader.common.login.LoginManager.Companion.initLogin
 import com.qq.reader.core.BaseApplication
 import com.qq.reader.core.http.HeadInterceptorManager
+import com.qq.reader.core.http.Http
+import com.qq.reader.provider.DataProviderConfig
 
 /**
  * Created by zhanglulu on 2020/3/16.
@@ -18,6 +20,9 @@ class MyApp: BaseApplication() {
         initLogin()
         //添加统一header到拦截器管理类中，此初始化要在多进程之前，保证多进程正常能通过拦截器获取登录态
         HeadInterceptorManager.getInstance().add(HeadInterceptor())
-
+        DataProviderConfig.init(this) { url, requestMethod, contentType, requestContent ->
+            Http.doRequest(url, requestContent, false, requestMethod, null, contentType, null, null)
+            ""
+        }
     }
 }
