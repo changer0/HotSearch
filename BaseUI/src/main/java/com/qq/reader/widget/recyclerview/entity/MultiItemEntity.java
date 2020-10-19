6ba9d801dd219e31ca938c6d7c7004739f9cdaf9
zhanglulu <zhanglulu@yuewen.com>
2020-10-19 21:53:43 +0800

    [commit] update

--- BaseUI/src/main/java/com/qq/reader/widget/recyclerview/entity/MultiItemEntity.java
+++
@@ -1,8 +0,0 @@
-package com.qq.reader.widget.recyclerview.entity;
-
-/**
- * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
- */
-public interface MultiItemEntity {
-    int getItemType();
-}
