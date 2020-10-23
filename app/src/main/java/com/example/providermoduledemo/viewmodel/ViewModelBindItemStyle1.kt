59706e671c661d282a5f9b57f3b0d1ff74407ed1
zhanglulu <zhanglulu@yuewen.com>
2020-10-23 23:31:49 +0800

    [update] 去掉 Map 实现，感觉这样会清爽一些

--- app/src/main/java/com/example/providermoduledemo/viewmodel/ViewModelBindItemStyle1.kt
+++ app/src/main/java/com/example/providermoduledemo/viewmodel/ViewModelBindItemStyle1.kt
@@ -4,8 +4,6 @@ import android.app.Activity
 import com.example.providermoduledemo.R
 import com.example.providermoduledemo.Utils
 import com.qq.reader.provider.BaseViewBindModelItem
-import com.qq.reader.provider.viewmodel.IModel
-import com.qq.reader.provider.viewmodel.IView
 import com.qq.reader.widget.recyclerview.base.BaseViewHolder
 
 class ViewModelBindItemStyle1 : BaseViewBindModelItem<ViewModelResponseDataBean.ItemData, BaseViewHolder>() {
@@ -16,12 +14,12 @@ class ViewModelBindItemStyle1 : BaseViewBindModelItem<ViewModelResponseDataBean.
     override fun buildViewModelMap(
         holder: BaseViewHolder,
         activity: Activity,
-        viewModelMap: MutableMap<IView<out IModel>, IModel>
+        viewModelList: MutableList<VM>
     ) {
         for (book in mItemData.bookList!!.withIndex()) {
             val view = holder.getView<SingleBookView>(Utils.getResIdByString("singleBook${book.index}", R.id::class.java))
             val model = SingleBookModel(book.value.name, book.value.content)
-            viewModelMap[view] = model
+            viewModelList.add(VM(view, model))
         }
     }
 
