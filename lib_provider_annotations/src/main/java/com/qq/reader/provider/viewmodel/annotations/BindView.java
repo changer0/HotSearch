65f714a3e52fafad47d22875b2a0033224600199
p_zlulzhang <zhanglulu@yuewen.com>
2020-10-28 11:19:15 +0800

    [commit] annotations 引入 依赖库

--- lib_provider_annotations/src/main/java/com/qq/reader/provider/viewmodel/annotations/BindView.java
+++ lib_provider_annotations/src/main/java/com/qq/reader/provider/viewmodel/annotations/BindView.java
@@ -1,5 +1,7 @@
 package com.qq.reader.provider.viewmodel.annotations;
 
+import androidx.annotation.IdRes;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -14,5 +16,5 @@ import java.lang.annotation.Target;
 //定该注解只能使用在类上：因为我们的功能是注册接口和实现类的关系。
 @Target(value = ElementType.TYPE)
 public @interface BindView {
-    int value() default 0;
+    @IdRes int value() default 0;
 }
