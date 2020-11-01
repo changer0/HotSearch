5487c959d9cc29d5f67dbe177ac1cc78257e72d9
zhanglulu <zhanglulu@yuewen.com>
2020-11-01 09:49:41 +0800

    [update] update

--- provider_viewmodel/src/main/java/com/qq/reader/provider/viewmodel/BaseViewBindModelItem.java
+++ provider_viewmodel/src/main/java/com/qq/reader/provider/viewmodel/BaseViewBindModelItem.java
@@ -49,7 +49,7 @@ public abstract class BaseViewBindModelItem
             }
             IViewModel value = viewModelEntry.getValue();
             if (value == null) {
-                Logger.e(TAG, "当前 Model 为空：" + value + " 对应的生成类中已经做了非空判断，理论上不应该出现为空的情况！！！");
+                Logger.e(TAG, "当前 ViewModel 为空：" + value);
                 continue;
             }
             ((IView) view).setModel(viewModelEntry.getValue());
