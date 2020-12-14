10eb34a32f40a7102b1b2bc07f32b9e98080493a
zhanglulu <zhanglulu@yuewen.com>
2020-12-14 14:16:39 +0800

    [commit] 暂时大刀阔斧的改

---
+++ provider_page/src/main/java/com/yuewen/dataprovider/page/IPage.java
@@ -0,0 +1,25 @@
+package com.yuewen.dataprovider.page;
+
+import android.os.Bundle;
+
+import com.qq.reader.provider.ProviderLiveData;
+import com.yuewen.dataprovider.page.IPageInfo;
+
+/**
+ * 页面构建器
+ */
+public interface IPage {
+
+    /**
+     * 加载 Page 数据
+     * @param params
+     * @return
+     */
+    ProviderLiveData loadPageData(Bundle params);
+
+    /**
+     * 构建 Page 信息 {并不是所有的场景都需要 PageInfo}
+     * @return
+     */
+    default IPageInfo buildPageInfo() {return null;}
+}
---
+++ provider_page/src/main/java/com/yuewen/dataprovider/page/IPageInfo.java
@@ -0,0 +1,8 @@
+package com.yuewen.dataprovider.page;
+
+/**
+ * @author zhanglulu
+ * @date : 2020/12/14 2:07 PM
+ */
+public interface IPageInfo {
+}
