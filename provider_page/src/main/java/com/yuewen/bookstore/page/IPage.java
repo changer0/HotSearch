74f5d79ac58b90777bdb3639f94d60c8a502ce47
zhanglulu <zhanglulu@yuewen.com>
2020-12-14 13:03:42 +0800

    [commit] 修复编译问题

---
+++ provider_page/src/main/java/com/yuewen/bookstore/page/IPage.java
@@ -0,0 +1,24 @@
+package com.yuewen.bookstore.page;
+
+import android.os.Bundle;
+
+import com.qq.reader.provider.ProviderLiveData;
+
+/**
+ * 页面构建器
+ */
+public interface IPage {
+
+    /**
+     * 加载 Page 数据
+     * @param params
+     * @return
+     */
+    ProviderLiveData loadPageData(Bundle params);
+
+    /**
+     * 构建 Page 信息 {并不是所有的场景都需要 PageInfo}
+     * @return
+     */
+    default PageInfo buildPageInfo() {return new PageInfo.Builder().build();}
+}
---
+++ provider_page/src/main/java/com/yuewen/bookstore/page/IPageFactory.java
@@ -0,0 +1,8 @@
+package com.yuewen.bookstore.page;
+
+/**
+ * ProviderGenerator 管理类，子类由注解处理器自动生成
+ */
+public interface IPageFactory {
+    IPage getPage(String type);
+}
