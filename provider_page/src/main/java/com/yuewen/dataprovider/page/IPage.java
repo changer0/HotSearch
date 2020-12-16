b3484d673eca6cd32842969d09b355fb92df19f5
zhanglulu <zhanglulu@yuewen.com>
2020-12-16 13:13:41 +0800

    [commit] 已调整

--- provider_page/src/main/java/com/yuewen/dataprovider/page/IPage.java
+++ provider_page/src/main/java/com/yuewen/dataprovider/page/IPage.java
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment;
 
 import com.qq.reader.provider.ProviderLiveData;
 
+import java.util.Map;
+
 /**
  * 页面构建器
  */
@@ -19,10 +21,14 @@ public interface IPage {
     ProviderLiveData loadPageData(Bundle params);
 
     /**
-     * 构建 Page 信息 {并不是所有的场景都需要 PageInfo}
-     * @return
+     * 构建 Fragment 参数信息 {并不是所有的场景都需要 PageInfo}
+     * @return 参数 Map
      */
-    default IPageInfo buildPageInfo() {return null;}
+    default Map<String, Object> getFragmentArguments() {return null;}
 
-    default Class<? extends Fragment> getFragment() {return null;}
+    /**
+     * 每个 Page 绑定一个 Fragment！
+     * @return Fragment 类
+     */
+    default Class<? extends Fragment> getFragment() { return null; }
 }
