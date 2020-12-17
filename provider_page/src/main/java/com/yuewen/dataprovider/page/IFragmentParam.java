0ab2f07108c4eb134675c067c28b0418e28538ce
zhanglulu <zhanglulu@yuewen.com>
2020-12-17 10:20:06 +0800

    [commit] 书城逻辑优化

--- provider_page/src/main/java/com/yuewen/dataprovider/page/IFragmentParam.java
+++ provider_page/src/main/java/com/yuewen/dataprovider/page/IFragmentParam.java
@@ -5,5 +5,5 @@ package com.yuewen.dataprovider.page;
  * @author zhanglulu
  * @date : 2020/12/14 2:07 PM
  */
-public interface IPageInfo {
+public interface IFragmentParam {
 }
--- provider_page/src/main/java/com/yuewen/dataprovider/page/IPage.java
+++ provider_page/src/main/java/com/yuewen/dataprovider/page/IPage.java
@@ -6,8 +6,6 @@ import androidx.fragment.app.Fragment;
 
 import com.qq.reader.provider.ProviderLiveData;
 
-import java.util.Map;
-
 /**
  * 页面构建器
  */
@@ -24,7 +22,7 @@ public interface IPage {
      * 构建 Fragment 参数信息 {并不是所有的场景都需要 PageInfo}
      * @return 参数 Map
      */
-    default Map<String, Object> getFragmentArguments() {return null;}
+    default IFragmentParam getFragmentParam() {return null;}
 
     /**
      * 每个 Page 绑定一个 Fragment！
