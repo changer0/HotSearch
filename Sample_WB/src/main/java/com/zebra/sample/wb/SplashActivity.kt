2860ff1878e0e481a70639ea1cda16ca0915d0ae
zhanglulu <zhanglulu@yuewen.com>
2021-02-14 16:16:17 +0800

    修改配色

--- Sample_WB/src/main/java/com/zebra/sample/wb/SplashActivity.kt
+++ Sample_WB/src/main/java/com/zebra/sample/wb/SplashActivity.kt
@@ -4,17 +4,23 @@ import android.app.ActivityOptions
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import com.qq.reader.activity.ReaderBaseActivity
 import com.qq.reader.bookstore.BookStoreActivityLauncher
 import com.qq.reader.bookstore.LaunchParams
+import com.yuewen.reader.zebra.log.Logger
+import kotlinx.android.synthetic.main.activity_main.*
 
-
+private const val TAG = "SplashActivity"
 class SplashActivity : ReaderBaseActivity() {
+    val handler = Handler(Looper.getMainLooper())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Handler(Looper.getMainLooper()).postDelayed({
+        text.text = getString(R.string.splash_text, getString(R.string.app_name))
+
+        handler.postDelayed({
             BookStoreActivityLauncher.launch(this, Constant.WB_HOT_SEARCH,
                 LaunchParams.Builder()
                     .setTitle("微博热搜榜")
@@ -23,8 +29,20 @@ class SplashActivity : ReaderBaseActivity() {
                 //转场动画 https://blog.csdn.net/w630886916/article/details/78319502
                 ActivityOptions.makeSceneTransitionAnimation(this, findViewById(R.id.img), "anim_weibo_img")
                     .toBundle())
-            finish()
+            //跳转之后消失
+            handler.postDelayed({finish()}, 2000)
         }, 500)
 
     }
+
+//    override fun onStop() {
+//        super.onStop()
+//        Logger.d(TAG, "onStop")
+//    }
+//
+//    override fun onRestart() {
+//        super.onRestart()
+//        Logger.d(TAG, "onRestart")
+//        finish()
+//    }
 }
