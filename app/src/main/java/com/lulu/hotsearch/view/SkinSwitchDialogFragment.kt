ceaba3a1958f6ea031f73ca1e091622cef08c265
zhanglulu <zhanglulu@yuewen.com>
2021-02-24 13:16:41 +0800

    垃圾代码>..<

--- app/src/main/java/com/lulu/hotsearch/view/SkinSwitchDialogFragment.kt
+++ app/src/main/java/com/lulu/hotsearch/view/SkinSwitchDialogFragment.kt
@@ -1,6 +1,27 @@
 package com.lulu.hotsearch.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.lulu.basic.view.BaseDialogFragment
+import com.lulu.hotsearch.R
+import kotlinx.android.synthetic.main.hot_search_switch_skin.*
 
 class SkinSwitchDialogFragment: BaseDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.hot_search_switch_skin, null)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+//        recyclerView.adapter =
+    }
 }
