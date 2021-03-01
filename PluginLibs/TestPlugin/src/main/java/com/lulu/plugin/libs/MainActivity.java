cfe521cc60870e1a04e783290bce2e793289ba4d
changer0 <941007zll>
2021-03-01 18:28:02 +0800

    添加插件混淆配置混淆配置

---
+++ PluginLibs/TestPlugin/src/main/java/com/lulu/plugin/libs/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lulu.plugin.libs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * PlaceHolder Activity
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- PluginLibs/TestPlugin/src/main/java/com/lulu/plugin/libs/TestPluginActivity.java
+++ PluginLibs/TestPlugin/src/main/java/com/lulu/plugin/libs/TestPluginActivity.java
@@ -2,8 +2,8 @@ package com.lulu.plugin.libs;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
-import com.lulu.basic.utils.ToastUtil;
 import com.lulu.plugin.PluginActivity;
 import com.lulu.plugin.test.R;
 
@@ -14,10 +14,12 @@ public class TestPluginActivity extends PluginActivity {
         setContentView(R.layout.test_plugin_layout);
         View viewById = findViewById(R.id.tvTest);
         if (viewById == null) {
-            ToastUtil.showShortToast("viewById 为空");
+            //ToastUtil.showShortToast("viewById 为空");
+            Toast.makeText(mProxyActivity, "viewById 为空", Toast.LENGTH_SHORT).show();
         } else {
             viewById.setOnClickListener(v -> {
-                ToastUtil.showShortToast("点击事件生效喽~");
+                //ToastUtil.showShortToast("点击事件生效喽~");
+                Toast.makeText(mProxyActivity, "点击事件生效喽, 插件成功了~", Toast.LENGTH_SHORT).show();
             });
         }
 
